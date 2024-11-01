ec07e93c057834bfc6f4e91dd4d344841a410ee2
Nishan <nishanbastola9095@gmail.com>
2024-11-01 11:03:39 +0545

    Cleaned Startup

--- Project.py
+++ Project.py
@@ -1,18 +1,28 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-df = pd.read_csv('startup_funding.csv')
+df = pd.read_csv('startup_cleaned.csv')
+def load_investor_details(investor):
+    st.title(investor)
+
 st.sidebar.title('Startup Funding Analysis')
+
 option = st.sidebar.selectbox('Select One', ['Overall Analysis', 'Start Up', 'Investor'])
 if option == 'Overall Analysis':
    st.title('Overall Analysis')
+
+   
+
 elif option == 'Start Up':
-    st.sidebar.selectbox('Select Startup', sorted(df['Startup Name'].unique().tolist()))
+    st.sidebar.selectbox('Select Startup', sorted(df['startup'].unique().tolist()))
     btn1 = st.sidebar.button('Find Start Up Details')
     st.title('Start Up Analysis')
 else:
-    df['Investors Name'] = df['Investors Name'].fillna('Undisclosed')  # data filtered gareko
-    st.sidebar.selectbox('Select Startup', sorted(df['Investors Name'].unique().tolist()))
+    df['investors'] = df['investors'].fillna('Undisclosed')  # data filtered gareko
+    selected_investor = st.sidebar.selectbox('Select Startup', sorted(set(df['investors'].str.split(',').sum())))
     bt2 = st.sidebar.button('Find Investors Details')
-    st.title('Inverstor Analysis')
+    if bt2:
+        load_investor_details(selected_investor)
+
+
 
