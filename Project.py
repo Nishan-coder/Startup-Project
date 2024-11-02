b3f986d9c042af320b4b31ab7662904da998b4de
Nishan <nishanbastola9095@gmail.com>
2024-11-02 17:37:23 +0545

    add bar chart to the investors

--- Project.py
+++ Project.py
@@ -1,9 +1,29 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
+st.set_page_config(layout="wide", page_title='Startup Analysis')
+
+
 df = pd.read_csv('startup_cleaned.csv')
 def load_investor_details(investor):
     st.title(investor)
+    #load the recent 5 investments of the investor
+    last5_df = df[df['investors'].str.contains('investor')].head()[['date', 'startup', 'vertical', 'city', 'round', 'amount']]
+    st.subheader('Most Recent Investments')
+    st.dataframe(last5_df)
+
+
+    col1, col2 = st.columns(2)
+    with col1:
+        #biggest investments
+        big_series= df[df['investors'].str.contains('investor')].groupby('startup')['amount'].sum().sort_values(ascending = False).head()
+        st.subheader('Most Recent Investments')
+        fig, ax = plt.subplots()
+        ax.bar(big_series.index, big_series.values)
+        st.pyplot(fig)
+
+
 
 st.sidebar.title('Startup Funding Analysis')
 
