6af13821396f7a58e894129baa4f90b2e4099980
Nishan <nishanbastola9095@gmail.com>
2024-11-02 18:21:07 +0545

    add pie chart to the investors

--- Project.py
+++ Project.py
@@ -2,25 +2,49 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
-st.set_page_config(layout="wide", page_title='Startup Analysis')
+st.set_page_config(page_title='Startup Analysis')
 
 
 df = pd.read_csv('startup_cleaned.csv')
 def load_investor_details(investor):
     st.title(investor)
     #load the recent 5 investments of the investor
-    last5_df = df[df['investors'].str.contains('investor')].head()[['date', 'startup', 'vertical', 'city', 'round', 'amount']]
+    last5_df = df[df['investors'].str.contains(investor)].head()[['date', 'startup', 'vertical', 'city', 'round', 'amount']]
     st.subheader('Most Recent Investments')
     st.dataframe(last5_df)
 
+    # biggest investments
+    big_series = df[df['investors'].str.contains(investor)].groupby('startup')['amount'].sum().sort_values(
+        ascending=False).head()
+    st.subheader('Most Recent Investments')
+    fig, ax = plt.subplots()
+    ax.bar(big_series.index, big_series.values)
+    st.pyplot(fig)
 
-    col1, col2 = st.columns(2)
+
+    col1, col2, col3 = st.columns(3)
     with col1:
-        #biggest investments
-        big_series= df[df['investors'].str.contains('investor')].groupby('startup')['amount'].sum().sort_values(ascending = False).head()
-        st.subheader('Most Recent Investments')
+        # invested by city
+        city_series = df[df['investors'].str.contains(investor)].groupby('city')['amount'].sum()
+        st.subheader('Sectors Invested In')
         fig, ax = plt.subplots()
-        ax.bar(big_series.index, big_series.values)
+        ax.pie(city_series, labels=city_series.index, autopct='%1.1f%%')
+        st.pyplot(fig)
+
+
+    with col2:
+        #invested by sectors
+        vertical_series = df[df['investors'].str.contains(investor)].groupby('vertical')['amount'].sum()
+        st.subheader('Sectors Invested In')
+        fig, ax = plt.subplots()
+        ax.pie(vertical_series, labels= vertical_series.index, autopct='%1.1f%%')
+        st.pyplot(fig)
+    with col3:
+        #invested by round
+        round_series = df[df['investors'].str.contains(investor)].groupby('round')['amount'].sum()
+        st.subheader('Rounds Invested In')
+        fig, ax = plt.subplots()
+        ax.pie(round_series, labels= round_series.index, autopct='%1.1f%%')
         st.pyplot(fig)
 
 
